50b449f2adf48285992872f7fd484d7d76e870c8
Silversoul28 <brooklyn-scott@outlook.com>
2024-01-31 14:12:01 -0800

    first commit

---
+++ src/test/java/ass1cmpt276/ass1/Ass1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ass1cmpt276.ass1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ass1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

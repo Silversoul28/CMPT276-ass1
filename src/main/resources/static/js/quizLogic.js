3a662f8e2987777afcdb53219ccbdbb8ff3ef27f
Silversoul28 <brooklyn-scott@outlook.com>
2024-02-03 18:40:51 -0800

    finalDraft

--- src/main/resources/static/js/quizLogic.js
+++ src/main/resources/static/js/quizLogic.js
@@ -1,4 +1,4 @@
-const questions =["What is 2 + 2", "What is 4 + 4", "What is 8 + 8"]
+const questions =["What is 2 + 2?", "What is 4 + 4?", "What is 8 + 8?"]
 const choice1=[6, 4, 3]
 const choice2=[4, 2, 9]
 const choice3=[3, 8, 10]
@@ -7,55 +7,36 @@ const answers =[1, 2, 3]
 var choice =[-1, -1, -1]
 var count =0;
 var score=0;
-//document.getElementById( 'next' ).style.display = 'none';
-//document.getElementsByClassName( 'right' ).style.display = 'none';
-
 
 
 var begin = document.querySelector('input[value="Begin"]')//grab the submit button
 begin.addEventListener('click', function(evt){
     evt.preventDefault()
-    console.log(document.getElementsByTagName("legend")[0].innerHTML)
+
     document.getElementsByTagName('legend')[0].innerHTML = "Question "+(count+1)
-
-    document.getElementById( 'begin' ).style.display = 'none';
-    document.getElementById( 'next' ).style.display = 'block';
-  
-
-    for(var i=0; i<4;i++){
-    document.getElementsByName('choiceN')[i].style.display='inline-block';
-    document.getElementsByName('choiceN')[i].style.marginLeft='50%';
-    }
-    //document.getElementsByClassName( 'center' )[0].style.display='block';
-
-
-
     document.querySelector('label[for="choice1"]').innerHTML = choice1[count]
     document.querySelector('label[for="choice2"]').innerHTML = choice2[count]
     document.querySelector('label[for="choice3"]').innerHTML = choice3[count]
     document.querySelector('label[for="choice4"]').innerHTML = choice4[count]
     document.getElementById('question').innerHTML = questions[count]
+
+    //document.getElementById( 'begin' ).style.display = 'none';
+    document.getElementById( 'start' ).style.display = 'none';
+    document.getElementById( 'next' ).style.display = 'block';
+    document.getElementById( 'quest' ).style.display = 'block';
+    document.getElementById( 'prev' ).style.display = 'none';
+    document.getElementsByTagName('legend')[0].style.display='block';
+    for(var i=0; i<4;i++){
+    document.getElementsByName('choiceN')[i].style.display='inline-block';
+    document.getElementsByName('choiceN')[i].style.marginLeft='50%';
+    }
 })
 
-var prev = document.querySelector('input[value="Previous"]')//grab the submit button
+var prev = document.querySelector('input[value="Previous"]')//grab the prev button
 prev.addEventListener('click', function(evt){
    evt.preventDefault()
  
-   for(var i=0; i<4;i++){
-    if(document.getElementsByClassName('choiceALL')[i].checked==true){
-      choice[count]=document.getElementsByClassName('choiceALL')[i].value;
-    }
-  }
-
-   if(count>0){
-    count--;
-    document.getElementsByClassName('choiceALL')[choice[count]].checked= true;
-   }
-   if(count==0) {
-    document.getElementById( 'prev' ).style.display = 'none';
-   }
-
-    document.getElementById('next').value="Next";
+    checkPagePrev();
 
     document.querySelector('label[for="choice1"]').innerHTML = choice1[count]
     document.querySelector('label[for="choice2"]').innerHTML = choice2[count]
@@ -67,16 +48,13 @@ prev.addEventListener('click', function(evt){
 
  })
 
-
- var next = document.querySelector('input[value="Next"]')//grab the submit button
+ var next = document.querySelector('input[value="Next"]')//grab the next button
  next.addEventListener('click',function(evt){
     evt.preventDefault()
 
     if(individualAns()){
 
-    document.getElementById( 'prev' ).style.display = 'block';
-
-    checkPage();
+    checkPageNext();
 
     document.querySelector('label[for="choice1"]').innerHTML = choice1[count]
     document.querySelector('label[for="choice2"]').innerHTML = choice2[count]
@@ -84,68 +62,77 @@ prev.addEventListener('click', function(evt){
     document.querySelector('label[for="choice4"]').innerHTML = choice4[count]
     document.getElementById('question').innerHTML = questions[count]
     document.getElementsByTagName('legend')[0].innerHTML = "Question "+(count+1)
-   
-    }
-    
-              //add a spot for caching past answers when the prev button is pressed
-        //and add one in the prev event listener as well
 
-        
-   
+    document.getElementById( 'prev' ).style.display = 'block';
+    }
   })
 
+ function checkPagePrev(){//checking and updating chosen values on the previous page and current page
 
- function checkPage(){
+  for(var i=0; i<4;i++){//keeps the button pressed on the page after if you've clicked it and then pressed previous
+    if(document.getElementsByClassName('choiceALL')[i].checked==true){
+      choice[count]=document.getElementsByClassName('choiceALL')[i].value;
+    }
+  }
 
+   if(count>0){
+    count--;
+    document.getElementsByClassName('choiceALL')[choice[count]].checked= true;//keeps the button you selected and came back to still pressed
+   }
+   if(count==0) {
+    document.getElementById( 'prev' ).style.display = 'none';//if its the first page remove previous button
+   }
+
+    document.getElementById('next').value="Next";//changes the button back to next if it was submit
+
+ }
+
+ function checkPageNext(){//checking and updating chosen values on the next page and current page
+
+  if((choice[count+1]!=-1)&&(count<2)){//if on the second to last page or less and you've already selected an answer on the next page, keeps that button selected
+    document.getElementsByClassName('choiceALL')[choice[count+1]].checked=true;
+  }
+  
   if(count <1){
      count++;
   }
   else if(count ==1){
       count ++;
-      document.getElementById('next').value="Submit";
+      document.getElementById('next').value="Submit";//if last page change next button to a submit button
   }
   else if(count==2){
      subAnswers();
   }
 }
 
-//for next button
-function individualAns(){
+function individualAns(){//records choices
 
 var clicked=false;
+
   for(var i=0; i<4;i++){
     if(document.getElementsByClassName('choiceALL')[i].checked==true){
-      choice[count]=document.getElementsByClassName('choiceALL')[i].value;
+      choice[count]=document.getElementsByClassName('choiceALL')[i].value;//loads choice into array
       clicked=true;
     }
-    document.getElementsByClassName('choiceALL')[i].checked=false;
+    document.getElementsByClassName('choiceALL')[i].checked=false;//resets the buttons
   }
-  if((choice[count+1]!=-1)&&(count<2)){
-    document.getElementsByClassName('choiceALL')[choice[count+1]].checked=true;
-  }
-  if(clicked!=true){
+
+  if(clicked!=true){//if no selection was detected
     alert("Select an Option Before Continuing");
     return 0;
   }
 return 1;
 } 
-//for prev button
-// function answerCache(){
 
-//   if(count!=0){
-//     document.getElementsByClassName('choiceC')[choice[count-1]].checked= true;
-//   }
-
-// }
-
-  function subAnswers(){
+function subAnswers(){//displays the answers and score
 
     document.getElementById("quiz").style.display='none';
     document.getElementById("answers").style.display='block';
     document.getElementById("score").style.display='block';
 
    for(var i=0; i<3;i++){
-
+    document.getElementsByClassName('questionNum')[i].innerHTML = "Question "+(i+1)
+    document.getElementsByClassName('questionn')[i].innerHTML = questions[i]
     document.getElementsByClassName('choiceC')[(i+1)*4].innerHTML = choice1[i]
     document.getElementsByClassName('choiceC')[((i+1)*4)+1].innerHTML = choice2[i]
     document.getElementsByClassName('choiceC')[((i+1)*4)+2].innerHTML = choice3[i]
@@ -168,28 +155,3 @@ return 1;
    document.getElementById('fraction').innerHTML=score+"/"+"3";
   }
 
- 
-
-
-  
-  //  ans.style.backroundColor='red';
-  //  ans.style.boxSizing='border-box';
-
-    //here goes function or class that will capture and record the answers
-  
-    //and another function that checks if the answers are correct
-  
-    //then probably a page refresh to or a div tha captures everything forr a display none
-    //and then display all the answers with html
-  
-  
-
-// left to do
-// the final page maybe do a refresh and show all the questions and answeers
-// and of course figure out how to actually save the answeers
-// and then figure  out how to compare and keep the answeers
-// not only for the score but to show each questoins right an d wrong answer
-
-
-// let var1 = document.getElementById("quiz");
-// var1;
